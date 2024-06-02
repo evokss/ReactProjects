f3ad75a3560ed98ac54dccfb12acb26ada2d1384
Eva <ekoss.prog@gmail.com>
2024-06-02 13:04:08 +0400

    Implement Navbar component with logo and project title

---
+++ airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav class="navbar">
+            <img src="../src/assets/airbnb-logo.png" className="nav--logo" />
+            <span>Eva's Projects</span>
+        </nav>
+    )
+}
