59be424bca6a448aa84ca53545a1dc9eb5204f04
Eva <ekoss.prog@gmail.com>
2025-01-23 20:58:22 +0400

    fix: update asset paths to reflect the new public location - Adjusted file imports and references to use the new path for assets in the public folder.

--- react-info-page/src/components/Navbar.jsx
+++ react-info-page/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from "react"
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <nav>
-            <img src="../src/assets/react-icon-small.png" className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">Eva's Projects</h4>
-        </nav>
-    )
+  return (
+    <nav>
+      <img src="/assets/react-icon-small.png" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">Eva's Projects</h4>
+    </nav>
+  );
 }
